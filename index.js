22f979ca2003d3b2abed00309980dce1763c6110
hkv.2ca11{mitten} <151836435+hkv2k11@users.noreply.github.com>
2025-02-20 13:37:49 +0700

    owo

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { fetchData, checkForUpdates } = require('./services/dataService');
 const { setupStartCommand } = require('./cmd/start');
 const { setupBotOnCommand } = require('./cmd/bot_on');
 const { setupRecallCommand } = require('./cmd/recall');
+const { setupAddKhoCloudCommand } = require('./cmd/add_khocloud'); // Thêm lệnh mới
 
 const TELEGRAM_TOKEN = '6783805716:AAFisKmrTvPfgM1YYzvY_o9bgOks6P2DDTk';
 const CHAT_ID = '5182125784';
@@ -13,6 +14,7 @@ const bot = new Telegraf(TELEGRAM_TOKEN);
 setupStartCommand(bot);
 setupBotOnCommand(bot, checkForUpdates);
 setupRecallCommand(bot, fetchData);
+setupAddKhoCloudCommand(bot); // Đăng ký lệnh add
 
 // Khởi động bot
 bot.launch();
